b2dff59ff584fb55909b98c4dbb35b946f11d8c8
Ione03 <suratiwan03@gmail.com>
2022-11-22 12:01:32 +0800

    Tambah template parler

--- menu/_version.py
+++ menu/_version.py
@@ -5,10 +5,7 @@ Provides menu version information.
 # This file is auto-generated! Do not edit!
 # Use `python -m incremental.update menu` to change this file.
 
-# run this:
-# python -m incremental.update menu --newversion=1.0.9
-
 from incremental import Version
 
-__version__ = Version("menu", 1, 0, 9)
+__version__ = Version("menu", 1, 0, 10)
 __all__ = ["__version__"]
